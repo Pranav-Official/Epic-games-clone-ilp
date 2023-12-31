a045cef7f2113003fa4cfd34104caa7a76ebbff0
Abin George <abin.george@experionglobal.local>
2023-12-31 16:51:02 +0530

    feat:firebase configuration and login function+ form validation and animation fixes

--- scripts/login_js/login.js
+++ scripts/login_js/login.js
@@ -32,18 +32,60 @@ document
   .addEventListener("click", function checkMail() {
     let email = document.getElementById("email-field").value;
     sessionStorage.setItem("email", email);
-    fetchSignInMethodsForEmail(auth, email)
-      .then((signInMethods) => {
-        if (signInMethods.length === 0) {
-          console.log(signInMethods);
-          console.log("Email is not registered.");
-        } else {
-          console.log("Email is already registered.");
-          console.log("Sign-in methods:", signInMethods);
-          window.location.href = "login_password.html";
-        }
-      })
-      .catch((error) => {
-        console.error("Error checking email:", error);
-      });
+    if (validateInput()) {
+      fetchSignInMethodsForEmail(auth, email)
+        .then((signInMethods) => {
+          if (signInMethods.length === 0) {
+            console.log(signInMethods);
+            console.log("Email is not registered.");
+          } else {
+            console.log("Email is already registered.");
+            console.log("Sign-in methods:", signInMethods);
+            window.location.href = "login_password.html";
+          }
+        })
+        .catch((error) => {
+          console.error("Error checking email:", error);
+        });
+    }
   });
+
+const validateInput = () => {
+  let emailInput = document.getElementById("email-field");
+  let submitDiv = document.getElementById("continue-div");
+  let isvalidDiv = document.getElementById("invalid-mail");
+
+  var isValidEmail = isValidEmailFormat(emailInput.value);
+  if (emailInput.value.trim() === "") {
+    emailInput.style.borderColor = "#DE3341";
+    submitDiv.classList.remove("continue-button");
+    isvalidDiv.textContent = "Required";
+    isvalidDiv.removeAttribute("hidden");
+    return false;
+  } else if (!isValidEmail) {
+    emailInput.style.borderColor = "#DE3341";
+    submitDiv.classList.remove("continue-button");
+    isvalidDiv.textContent = "Invalid email";
+    isvalidDiv.removeAttribute("hidden");
+    return false;
+  } else {
+    console.log("valid");
+    emailInput.style.borderColor = "";
+    isvalidDiv.setAttribute("hidden", "true");
+    submitDiv.classList.add("continue-button");
+    return true;
+  }
+};
+
+function isValidEmailFormat(email) {
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+document.getElementById("email-field").addEventListener("input", validateInput);
+
+document.getElementById("email-field").addEventListener("input", function () {
+  var placeholderLabel = document.querySelector(".placeholder-label");
+  placeholderLabel.style.top = this.value ? "20%" : "50%";
+  placeholderLabel.style.fontSize = this.value ? "12px" : "14px";
+});
