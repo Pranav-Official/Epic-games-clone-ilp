694241879c8f1b25e89d59b90c34bed659d9328c
Aneeka Geo <aneeka.geo@experionglobal.local>
2024-01-03 12:10:50 +0530

    Feat:Filter by genre

--- scripts/browsepage.js
+++ scripts/browsepage.js
@@ -2,59 +2,147 @@ import fetchData from "./rawgfetchGamesdata.js";
 import getPrice from "./getprice.js";
 import { API_KEY } from "../environment.js";
 
-const loadBrowsePage = async () => {
-  const browsepage_gamecards = document.getElementById("browsepage-game-cards");
-  const node = browsepage_gamecards.querySelector(".game-card");
-  const gamesCardCopy = node.cloneNode(true);
-  //   browsepage_gamecards.innerHTML = "";
-  const parameterList = [
-    ["platforms", "4"],
-    ["ordering", "-metacritic"],
-  ];
+const loadBrowsePage = async (parameterList = null) => {
+  if (parameterList == null) {
+    parameterList = [
+      ["platforms", "4"],
+      ["ordering", "-metacritic"],
+    ];
+  }
 
   try {
+    const container = document.getElementById("browsepage-game-cards"); // Replace 'game-card-container' with the actual ID or selector for your container
     const data = await fetchData(API_KEY, parameterList);
-
-    // Check if there are at least 5 results
-    if (data["results"].length > 0) {
-      // Loop through the first 5 results and update the game cards
-      for (let i = 0; i < data["results"].length; i++) {
-        const gameCard =
-          browsepage_gamecards.getElementsByClassName("game-card")[i];
-
-        // Update game card content with fetched data
-        gameCard.querySelector(".game-card-image img").src =
-          data["results"][i].background_image;
-        gameCard.querySelector(".base-game").textContent = "BASE GAME";
-        if (data["results"][i].name.length > 20) {
-          gameCard.querySelector(".game-card-title").textContent =
-            data["results"][i].name.slice(0, 20) + "...";
-        } else {
-          gameCard.querySelector(".game-card-title").textContent =
-            data["results"][i].name;
-        }
-        let prices = await getPrice(data["results"][i].slug);
-        // let prices = null;
-        if (prices === null) {
-          gameCard.querySelector(".game-card-discount").remove(); // You may need to update this value
-          gameCard.querySelector(".game-card-previous-price").remove(); // You may need to update this value
-          gameCard.querySelector(".game-card-current-price").remove(); // You may need to update this value
-        } else {
-          console.log(prices);
-          gameCard.querySelector(".game-card-discount").textContent =
-            "-" + Math.trunc(prices.calculatedDiscount) + "%"; // You may need to update this value
-          gameCard.querySelector(".game-card-previous-price").textContent =
-            "₹" + prices.retailPrice; // You may need to update this value
-          gameCard.querySelector(".game-card-current-price").textContent =
-            "₹" + prices.salePrice; // You may need to update this value
-        }
-        browsepage_gamecards.appendChild(gameCard);
+    container.innerHTML = "";
+    for (let i = 0; i < data.results.length; i++) {
+      const gameData = data.results[i];
+      // const prices = await getPrice(gameData.slug);
+      const prices = null;
+      let gameCardHTML = ``;
+      if (prices === null) {
+        gameCardHTML = `
+      <div class="game-card">
+        <div class="game-card-image">
+          <img src="${gameData.background_image}" alt="${gameData.name}" />
+        </div>
+        <p class="base-game">BASE GAME</p>
+        <h3 class="game-card-title">${gameData.name}</h3>
+        <div class="game-card-footer">
+        </div>
+      </div>
+    `;
+      } else {
+        gameCardHTML = `
+      <div class="game-card">
+        <div class="game-card-image">
+          <img src="${gameData.background_image}" alt="${gameData.name}" />
+        </div>
+        <p class="base-game">BASE GAME</p>
+        <h3 class="game-card-title">${gameData.name}</h3>
+        <div class="game-card-footer">
+          <p class="game-card-discount">-${prices.calculatedDiscount}%</p>
+          <h4 class="game-card-previous-price">&#8377;${prices.retailPrice}</h4>
+          <h4 class="game-card-current-price">&#8377;${prices.salePrice}</h4>
+        </div>
+      </div>
+    `;
       }
-    } else {
-      console.error("Insufficient data to update game cards.");
+
+      container.innerHTML += gameCardHTML;
     }
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 };
+
+const filterBrowsePageByGenre = (genre) => {
+  const parameterList = [
+    ["platforms", "4"],
+    ["genres", genre],
+    ["ordering", "-metacritic"],
+  ];
+  loadBrowsePage(parameterList);
+  // console.log(genre);
+};
+
 loadBrowsePage();
+
+document.getElementById("filter_by_action").addEventListener("click", () => {
+  filterBrowsePageByGenre("action");
+});
+
+document.getElementById("filter_by_indie").addEventListener("click", () => {
+  filterBrowsePageByGenre("indie");
+});
+
+document.getElementById("filter_by_adventure").addEventListener("click", () => {
+  filterBrowsePageByGenre("adventure");
+});
+
+document.getElementById("filter_by_rpg").addEventListener("click", () => {
+  filterBrowsePageByGenre("rpg");
+});
+
+document.getElementById("filter_by_strategy").addEventListener("click", () => {
+  filterBrowsePageByGenre("strategy");
+});
+
+document.getElementById("filter_by_shooter").addEventListener("click", () => {
+  filterBrowsePageByGenre("shooter");
+});
+
+document.getElementById("filter_by_casual").addEventListener("click", () => {
+  filterBrowsePageByGenre("casual");
+});
+
+document.getElementById("filter_by_puzzle").addEventListener("click", () => {
+  filterBrowsePageByGenre("puzzle");
+});
+
+document.getElementById("filter_by_arcade").addEventListener("click", () => {
+  filterBrowsePageByGenre("arcade");
+});
+
+document
+  .getElementById("filter_by_platformer")
+  .addEventListener("click", () => {
+    filterBrowsePageByGenre("platformer");
+  });
+
+document
+  .getElementById("filter_by_massive-multiplayer")
+  .addEventListener("click", () => {
+    filterBrowsePageByGenre("massive-multiplayer");
+  });
+
+document.getElementById("filter_by_racing").addEventListener("click", () => {
+  filterBrowsePageByGenre("racing");
+});
+
+document.getElementById("filter_by_sports").addEventListener("click", () => {
+  filterBrowsePageByGenre("sports");
+});
+
+document.getElementById("filter_by_fighting").addEventListener("click", () => {
+  filterBrowsePageByGenre("fighting");
+});
+
+document.getElementById("filter_by_family").addEventListener("click", () => {
+  filterBrowsePageByGenre("family");
+});
+
+document
+  .getElementById("filter_by_board-games")
+  .addEventListener("click", () => {
+    filterBrowsePageByGenre("board-games");
+  });
+
+document
+  .getElementById("filter_by_educational")
+  .addEventListener("click", () => {
+    filterBrowsePageByGenre("educational");
+  });
+
+document.getElementById("filter_by_card").addEventListener("click", () => {
+  filterBrowsePageByGenre("card");
+});
