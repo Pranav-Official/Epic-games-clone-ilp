99e394eeb21b56dbbd3a121259c83e21bc8eb63b
Pranav P Prasanth <pranav.prasanth@experionglobal.local>
2024-01-03 13:03:30 +0530

    feat:tried dynamic tripple list

--- scripts/discover/discover.js
+++ scripts/discover/discover.js
@@ -214,9 +214,9 @@ const populateSalesHighlights = async (title, id, parameterList) => {
         gameCard.querySelector(".game-card-image img").src =
           data["results"][i].background_image;
         gameCard.querySelector(".base-game").textContent = "BASE GAME";
-        if (data["results"][i].name.length > 20) {
+        if (data["results"][i].name.length > 25) {
           gameCard.querySelector(".game-card-title").textContent =
-            data["results"][i].name.slice(0, 20) + "...";
+            data["results"][i].name.slice(0, 25) + "...";
         } else {
           gameCard.querySelector(".game-card-title").textContent =
             data["results"][i].name;
@@ -245,6 +245,81 @@ const populateSalesHighlights = async (title, id, parameterList) => {
   }
 };
 
+const populateTrippleList = async (
+  titles,
+  id,
+  parameterList1,
+  parameterList2,
+  parameterList3
+) => {
+  const tripleListContainer = document.getElementById(id);
+  if (!tripleListContainer) {
+    const node = document.getElementsByClassName("triple-list")[0];
+    tripleListContainer = node.cloneNode(true);
+  }
+  const class_list = [
+    ".left-tripple-list",
+    ".middle-tripple-list",
+    ".right-tripple-list",
+  ];
+
+  let trippleListItemTemplate = `<div class="tripple-list-items">
+            <div class="tripple-item">
+              <div class="tripple-item-image"></div>
+              <div class="tripple-item-text">
+                <h4>Destiny 2</h4>
+                <div class="game-card-footer">
+                  <p class="game-card-discount">-5%</p>
+                  <h4 class="game-card-previous-price">₹1,429</h4>
+                  <h4 class="game-card-current-price">₹1,286</h4>
+                </div>
+              </div>
+            </div>
+          </div>`;
+
+  const parameterMasterList = [parameterList1, parameterList2, parameterList3];
+  for (let i = 0; i < 3; i++) {
+    tripleListContainer.querySelector(".tripple-list-title h3").textContent =
+      titles[i];
+    try {
+      const data = await fetchData(API_KEY, parameterMasterList[i]);
+      let tripleList = tripleListContainer.getElementsByClassName(
+        class_list[i]
+      )[0];
+      tripleList.innerHTML = "";
+      for (let j = 0; j < data["results"].length; j++) {
+        tripleList.innerHTML += trippleListItemTemplate;
+        let gameCard = tripleList.querySelectorAll(".tripple-item")[j];
+        gameCard.querySelector(
+          ".tripple-item-image"
+        ).style.backgroundImage = `url(${data["results"][j].background_image})`;
+        gameCard.querySelector(".tripple-item-text h4").textContent =
+          data["results"][j].name;
+        let prices = await getPrice(data["results"][j].slug);
+        if (prices === null) {
+          gameCard.querySelector(".game-card-discount").remove(); // You may need to update this value
+          gameCard.querySelector(".game-card-previous-price").remove(); // You may need to update this value
+          gameCard.querySelector(".game-card-current-price").remove(); // You may need to update this value
+        } else {
+          gameCard.querySelector(".game-card-discount").textContent =
+            "-" + Math.trunc(prices.calculatedDiscount) + "%"; // You may need to update this value
+          gameCard.querySelector(".game-card-previous-price").textContent =
+            "₹" + prices.retailPrice; // You may need to update this value
+          gameCard.querySelector(".game-card-current-price").textContent =
+            "₹" + prices.salePrice; // You may need to update this value
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  try {
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
 bestGamesOf2023();
 // reecentlyUpadatedList();
 // recentlyReleasedList();
@@ -263,3 +338,20 @@ populateSalesHighlights("Recently Released", "recentlyReleased-highlights", [
   ["platforms", "4"],
   ["ordering", "-released"],
 ]);
+
+// populateTrippleList(
+//   ["Top Rated", "Recently Updated", "Recently Released"],
+//   "tripple-list-container-1",
+//   [
+//     ["platforms", "4"],
+//     ["ordering", "-metacritic"],
+//   ],
+//   [
+//     ["platforms", "4"],
+//     ["ordering", "-metacritic"],
+//   ],
+//   [
+//     ["platforms", "4"],
+//     ["ordering", "-metacritic"],
+//   ]
+// );
