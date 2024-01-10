ee332d5cfc20407c5476f097976f01bda4611c07
Abin George <abin.george@experionglobal.local>
2024-01-10 08:40:44 +0530

    forgot password complete

--- scripts/login_js/login_forgot_password.js
+++ scripts/login_js/login_forgot_password.js
@@ -8,22 +8,67 @@ import {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const email = document.querySelector("#email-field-locked").value;
 
 const changePassWord = () => {
-  sendPasswordResetEmail(auth, email)
-    .then(() => {
-      console.log("password reset mail sent");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  if (validateInput()) {
+    const email = document.querySelector("#email-field-forgot").value;
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("password reset mail sent");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(`Password reset error: ${errorCode} - ${errorMessage}`);
+      });
+  }
 };
 
 document
   .querySelector("#sendmail-button")
   .addEventListener("click", changePassWord);
+
+const validateInput = () => {
+  let emailInput = document.getElementById("email-field-forgot");
+  let emailBox = document.querySelector(".email-input-forgot-inner");
+  let submitDiv = document.getElementById("send-mail-div");
+  let isvalidDiv = document.getElementById("invalid-mail");
+  let continueLink = document.getElementById("sendmail-button");
+
+  let isValidEmail = isValidEmailFormat(emailInput.value);
+  if (emailInput.value.trim() === "") {
+    continueLink.style.cursor = "default";
+    emailBox.style.borderColor = "#DE3341";
+    submitDiv.classList.remove("sendmail-button");
+    isvalidDiv.textContent = "Required";
+    isvalidDiv.removeAttribute("hidden");
+    return false;
+  } else if (!isValidEmail) {
+    continueLink.style.cursor = "default";
+    emailBox.style.borderColor = "#DE3341";
+    submitDiv.classList.remove("sendmail-button");
+    isvalidDiv.textContent = "Invalid email";
+    isvalidDiv.removeAttribute("hidden");
+    return false;
+  } else {
+    console.log("valid");
+    continueLink.style.cursor = "pointer";
+    emailBox.style.borderColor = "";
+    isvalidDiv.setAttribute("hidden", "true");
+    submitDiv.classList.add("sendmail-button");
+    return true;
+  }
+};
+
+//funcion to check email format
+function isValidEmailFormat(email) {
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+document
+  .getElementById("email-field-forgot")
+  .addEventListener("input", validateInput);
 
 //function to animate password field placeholder text
 document
